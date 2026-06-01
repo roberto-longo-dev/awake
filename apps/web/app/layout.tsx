3bfe4b3a64dbaa87b81c4ea73848078359134d50
Roberto Longo <roberto.longo.pe@gmail.com>
2026-06-02 02:21:26 +0300

    feat: adjust script in head as first

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import "./globals.css";
@@ -33,10 +32,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-        <Script
-          src="https://app.termly.io/resource-blocker/9148a8c2-ae42-4785-8e1b-4c7bb7f86780?autoBlock=on"
-          strategy="beforeInteractive"
-        />
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script src="https://app.termly.io/resource-blocker/9148a8c2-ae42-4785-8e1b-4c7bb7f86780?autoBlock=on" />
       </head>
       <body className="min-h-full flex flex-col">
           <Header />
