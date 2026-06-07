811af8f2864ac91df93b6d5697fe43a3bc996538
Roberto Longo <roberto.longo.pe@gmail.com>
2026-06-07 16:27:42 +0300

    feat: prisma refinement

--- apps/web/lib/prisma.ts
+++ apps/web/lib/prisma.ts
@@ -1,14 +1,18 @@
 import { PrismaClient } from "@prisma/client"
 
-// Prevent multiple instances of Prisma Client in development
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+    prisma: PrismaClient | undefined
 }
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query"] : [],
-  })
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        datasources: {
+            db: {
+                url: process.env.DATABASE_URL,
+            },
+        },
+        log: process.env.NODE_ENV === "development" ? ["query"] : [],
+    })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
